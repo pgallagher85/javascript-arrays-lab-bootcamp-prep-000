2072becd2ee4fabad43fecc524cfa2d0c4ff4aca
Patrick Gallagher <pgallagher85@gmail.com>
2017-11-02 00:25:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const app = "I don't do much."
 
-kittens = ["Milo", "Otis", "Garfield"]
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(array, name) {
+  array.push(name)
+  return array
+}
